153ac38953c4fd9d0c58f536f19be3b2dd64e8bd
LenaCS305 <lena.barrett23@kzoo.edu>
2026-05-01 18:49:23 -0400

    I updated my methods with the correct documentation

--- InputValidatorTest.java
+++ InputValidatorTest.java
@@ -3,30 +3,28 @@
  * Java applications. For more information check the complete tutorial
  * here: https://www.tutorialspoint.com/junit/index.htm
  */
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
+/*This class shows test cases for boolean methods "isValidPhoneNum" and "isValidImage"
+ *
+ * @author Lena Barrett, Son Le Dinh Truong, Cole F. Russell, and Daniel E. Celedon
+ * @version May 1, 2026
+ */
 public class InputValidatorTest {
-    
-    /*
-     * Example test method for a possible Name field validator method in your 
-     * InputValidator class.
-     * For more information about assertion tests check: 
-     * https://www.tutorialspoint.com/junit/junit_using_assertion.htm
-     */
+
     @Test
-    public void testNameFieldIsNotEmpty() {
-        assertTrue(InputValidator.isValidPhoneNum("13108061152"));
-        assertFalse(InputValidator.isValidPhoneNum("02118063152"));
-
-        // assertFalse (InputValidator.validateNameField("..."));
-        assertTrue(InputValidator.isValidImage("bird.png", 4));
-        assertFalse(InputValidator.isValidImage("bird.png", 5));
-        assertFalse(InputValidator.isValidImage("bird.gif", 3));
-
+    public void testNameFieldIsNotEmpty()
+    {
+        // isValidPhoneNum Test Cases
+        assertTrue(InputValidator.isValidPhoneNum("13109061152")); //returns true because 1 is the only valid country code for US phone number.
+        assertFalse(InputValidator.isValidPhoneNum("02118063152")); //returns false because 0 is an invalid country code.
+        assertFalse(InputValidator.isValidPhoneNum("1012345678")); //returns false because number is not long enough.@Te
+        //isValidImage Test Cases
+        assertTrue(InputValidator.isValidImage("bird.png", 4)); //returns true because the file type is <=4, and it is a valid file type.
+        assertFalse(InputValidator.isValidImage("bird.png", 5)); //returns false because the file type is > 4.
+        assertFalse(InputValidator.isValidImage("bird.gif", 3)); //returns false because it is an invalid file type.
 
     }
-    
-    // CREATE MORE TESTS HERE
-    
 }
