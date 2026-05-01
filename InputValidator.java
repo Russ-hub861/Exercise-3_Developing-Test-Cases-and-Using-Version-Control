e43c7acc31176bf806a7dbceeedacaba67d59b91
DanielC0410 <dan0410cel@gmail.com>
2026-05-01 16:13:36 -0400

    Adding my working test codes

--- InputValidator.java
+++ InputValidator.java
@@ -4,7 +4,82 @@
  * methods to validate inputs from the user. 
  */
 
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+
 public class InputValidator {
-    
+    static ArrayList<String> userNames = new ArrayList<>();
+    static ArrayList<String> acceptedCurr = new ArrayList<>(List.of("US DOLLAR", "CANADIAN DOLLAR", "EURO", "PESO", "BIT COIN"));
+
+    static LocalDate today = LocalDate.now();
+
+    public static boolean validateDescriptionField(String desc){
+       return !desc.isEmpty() && desc.length() < 1000;
+   }
+
+    public static boolean validateTitleField(String title){
+        return !title.isEmpty() && title.length() < 100;
+    }
+
+    public static boolean validateUserNameField(String user){
+
+        if(!user.isEmpty() && user.length() <= 20 && !userNames.contains(user) )
+        {
+            userNames.add(user);
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean validateDateOfBirthField(String year, String month, String day){
+        if(!year.isEmpty() && !month.isEmpty() && !day.isEmpty())
+        {
+            int numYear = Integer.parseInt(year);
+            int numMonth = Integer.parseInt(month);
+            int numDay = Integer.parseInt(day);
+
+            LocalDate birthDay = LocalDate.of(numYear, numMonth, numDay);
+
+            if(birthDay.isAfter(today))
+                return false;
+
+            return Period.between(birthDay, today).getYears() >= 18;
+        }
+
+        return false;
+    }
+
+    public static boolean validateCreationDateField(String year, String month, String day){
+        if(!year.isEmpty() && !month.isEmpty() && !day.isEmpty()) {
+            int numYear = Integer.parseInt(year);
+            int numMonth = Integer.parseInt(month);
+            int numDay = Integer.parseInt(day);
+
+            LocalDate creationDate = LocalDate.of(numYear, numMonth, numDay);
+
+            return !creationDate.isAfter(today);
+        }
+        return false;
+    }
+
+    public static boolean validatePriceField(String currency, String price){
+
+       if(price.isEmpty() && currency.isEmpty()){
+           return true;
+       }
+       else if((price.isEmpty() && !currency.isEmpty()) || (!price.isEmpty() && currency.isEmpty()))
+           return false;
+
+       float num = Float.parseFloat(price);
+
+       if(acceptedCurr.contains(currency.toUpperCase()) && num >= 0.00)
+           return true;
+       return false;
+    }
+
+
 
 }
+
--- InputValidatorTest.java
+++ InputValidatorTest.java
@@ -4,22 +4,84 @@
  * here: https://www.tutorialspoint.com/junit/index.htm
  */
 import org.junit.Test;
+
+import static java.util.Locale.US;
 import static org.junit.Assert.*;
+
 
 public class InputValidatorTest {
     
     /*
-     * Example test method for a possible Name field validator method in your 
+     * Example test method for a possible Name field validator method in your
      * InputValidator class.
-     * For more information about assertion tests check: 
+     * For more information about assertion tests check:
      * https://www.tutorialspoint.com/junit/junit_using_assertion.htm
      */
+
     @Test
-    public void testNameFieldIsNotEmpty() {
-        // assertTrue(InputValidator.validateNameField("..."));
-        // assertFalse (InputValidator.validateNameField("..."));
+    public void testDateOfBirthFieldIsValid() {
+
+        assertTrue(InputValidator.validateDateOfBirthField("2008", "01","04"));
+        assertFalse(InputValidator.validateDateOfBirthField("2010", "01","04"));
+        assertFalse(InputValidator.validateDateOfBirthField("2008", "07","04"));
+        assertFalse(InputValidator.validateDateOfBirthField("", "",""));
+        assertFalse(InputValidator.validateDateOfBirthField("2020", "", ""));
     }
-    
+
+    @Test
+    public void testDescriptionFieldIsValid() {
+
+        assertTrue(InputValidator.validateDescriptionField("Hello World"));
+        assertFalse(InputValidator.validateDescriptionField(""));
+        assertFalse(InputValidator.validateDescriptionField("Java is a high-level, object-oriented programming language" +
+                " widely used for building applications, from mobile apps to large-scale enterprise systems. It was designed with the principle of “write once, run anywhere,”" +
+                " meaning that compiled Java code can run on any device that has a Java Virtual Machine (JVM), making it highly portable. Java uses classes and objects as its " +
+                "core structure, which helps organize code in a modular and reusable way. It has strong memory management, automatic garbage collection, and a large standard library " +
+                "that supports data structures, file handling, networking, and graphical user interfaces. Common data structures in Java include arrays and ArrayLists, which are used to " +
+                "store collections of data. Java also supports control structures like loops, conditionals, and exception handling to manage program flow and errors. Because of its reliability, " +
+                "security features, and cross-platform capability, Java is widely used in software development, Android apps, web applications, and backend systems. Its syntax is similar to C++, " +
+                "but it removes many complex features to make programming safer and easier to learn for beginners while still being powerful enough for advanced developers building complex systems" +
+                " and scalable applications."));
+    }
+
+    @Test
+    public void testTitleFieldIsValid() {
+
+        assertTrue(InputValidator.validateTitleField("Hello World"));
+        assertFalse(InputValidator.validateTitleField(""));
+        assertFalse(InputValidator.validateTitleField("Java is a programming language used to " +
+                "build apps, websites, and software across many platforms." + "but it removes many complex features "));
+    }
     // CREATE MORE TESTS HERE
-    
+
+    @Test
+    public void testCreationDateFieldIsValid() {
+
+        assertTrue(InputValidator.validateCreationDateField("2008", "01","04"));
+        assertFalse(InputValidator.validateCreationDateField("2026", "07","04"));
+        assertFalse(InputValidator.validateCreationDateField("", "", ""));
+        assertFalse(InputValidator.validateCreationDateField("2020", "", ""));
+
+    }
+    @Test
+    public void testPriceFieldIsValid() {
+
+        assertTrue(InputValidator.validatePriceField("euro", "10.99"));
+        assertTrue(InputValidator.validatePriceField("", ""));
+        assertFalse(InputValidator.validatePriceField("US DOLLAR", ""));
+        assertFalse(InputValidator.validatePriceField("", "10.00"));
+        assertFalse(InputValidator.validatePriceField("Yen", "20.09"));
+    }
+
+    @Test
+    public void testUserNameFieldIsValid() {
+
+        assertFalse(InputValidator.validateUserNameField(""));
+        assertTrue(InputValidator.validateUserNameField("Daniel"));
+        assertFalse(InputValidator.validateUserNameField("Daniel"));
+        assertTrue(InputValidator.validateUserNameField("Son!"));
+
+    }
+
+
 }
