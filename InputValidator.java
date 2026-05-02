8e9707b85ae1c3c85cdde6eb61492e5e110ceb60
CLAY3106 <sherpin316@gmail.com>
2026-05-01 20:03:41 -0400

    Refactor input validation methods for clarity and efficiency

--- InputValidator.java
+++ InputValidator.java
@@ -10,107 +10,90 @@ import java.util.List;
 
 public class InputValidator 
 {
-InputValidator isValidName = new InputValidator(); 
-    //Function for checking if the first name input is valid
+    InputValidator isValidName = new InputValidator();
+    /**
+     * Validates the first name input.
+     *
+     * @param input the first name provided by the user
+     * @return true if the input is valid, false otherwise
+     */
     public static boolean isValidName(String firstname)
     {
-        String name = firstname;
-
         //makes sure the length is more than 2 characters
-        if(name.length() < 2)
-        {
-            System.out.println("Name must be more than 2 characters");
-            return false;
-        }
-        else 
-        {
-            return true;
-        }
+        return firstname.length() > 2;
     }
-    // Function to check if the postal code entered is valid 
-InputValidator isValidPostalCode = new InputValidator();
+    
+    /**
+     * Validates the postal code input.
+     *
+     * @param input the postal code provided by the user
+     * @return true if the input is valid, false otherwise
+     */
     public static boolean isValidPostalCode(String postalcode)
     {
-       String pCode = postalcode;
-       if(pCode == null || pCode.isEmpty())
+        if(postalcode == null || postalcode.isEmpty())
         {
             return true; // Because no value is also allowed
         }
-        if(pCode.length() == 5) // makes the only valid input 5 digits which is what all postal codes are
-            {
-                return true;
-            }
-        
-        else  
+        if(postalcode.length() == 5) // makes the only valid input 5 digits which is what all postal codes are
         {
-            System.out.println("Enter a valid postal code");
-            return false;
+            return true;
         }
+        return false;
     }
-    // Function to check whether the password entered is valid
+
+    /**
+     * Validates the password input.
+     *
+     * @param input the password provided by the user
+     * @return true if the input is valid, false otherwise
+     */
     InputValidator isValidPassWord = new InputValidator();
     public static boolean isValidPassWord(String passWord)
     {
-        String pass = passWord;
-                if(pass.isEmpty()) //if the password filed is empty send out a message
-                    {
-                        System.out.println("Field cannot be empty");
-                        return false; 
-                    }
-                if(pass.length() < 8) // Makes sure that the password is at least 8 characters long
-                    {
-                        return false;
-                    }
-                    boolean hasUpper = false;
-                    boolean hasLower = false;
-                    boolean hasDigit = false;
-
-        for(int i = 0; i < pass.length(); i++) // Iterates through the characters of the password
+        if(passWord.isEmpty()) //if the password filed is empty send out a message
             {
-
-                char ch = pass.charAt(i);
-                if(Character.isUpperCase(ch)) //Checks if the character at the point "ch" is capital
-                    {
-                        hasUpper = true;
-                    }
-                    else {
-                        System.out.println("Must have at least one capital letter");
-                    }
-                if(Character.isLowerCase(ch)) //Checks if the character at the point "ch" is lower case 
-                    {
-                        hasLower = true;
-                    }
-                    else {
-                        System.out.println("Must have at least one lower case letter");
-                    }
-
-                if(Character.isDigit(ch)) //Checks if the character at the point "ch" is a digit
-                    {
-                        hasDigit = true;
-                    }
-                    else {
-                        System.out.println("Must have at least one digit");
-                    }
+                return false; 
             }
-            return hasUpper && hasLower && hasDigit;
+        if(passWord.length() < 8) // Makes sure that the password is at least 8 characters long
+            {
+                return false;
+            }
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        boolean hasDigit = false;
+
+        for(int i = 0; i < passWord.length(); i++) // Iterates through the characters of the password
+        {
+            char ch = passWord.charAt(i);
+            if(Character.isUpperCase(ch)) //Checks if the character at the point "ch" is capital
+            {
+                hasUpper = true;
+            }
+            if(Character.isLowerCase(ch)) //Checks if the character at the point "ch" is lower case 
+            {
+                hasLower = true;
+            }
+            if(Character.isDigit(ch)) //Checks if the character at the point "ch" is a digit
+            {
+                hasDigit = true;
+            }
+        }
+        return hasUpper && hasLower && hasDigit;
     }
 
-    // Function to check whether the medium of a painting is valid
+    /**
+     * Validates the medium input.
+     *
+     * @param input the medium provided by the user
+     * @return true if the input is valid, false otherwise
+     */
     static ArrayList<String> acceptedMed = new ArrayList<>(List.of("paint", "graphite", "clay", "digital"));
-    InputValidator isValidMedium = new InputValidator();
     public static boolean isValidMedium(String pieceMedium)
     {
-        String medium = pieceMedium;
-        if(acceptedMed.contains(medium))
-            {
-                return true;
-            }
-        else{
-            return false;
-        }
-
+        return acceptedMed.contains(pieceMedium); //Checks if the input is in the list of accepted mediums
     }
-public class InputValidator {
+
     /**
      * Validates the last name input.
      *
